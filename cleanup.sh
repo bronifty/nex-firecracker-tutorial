492eda6663b0c05bfb2e6bf8f31efdc2bfd42fe6
bronifty <bronifty@gmail.com>
2024-01-29 04:43:58 +0000

    this

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source variables.sh
 
-rm -rf ubuntu* vmlinux* release* firecracker-repo nex-repo firecracker-config* simple.json go.* *.go nex-firecracker
+rm -rf ubuntu* vmlinux* release* firecracker-repo nex-repo firecracker-config* simple.json go.* *.go nex-firecracker echoservice
 
 rm -rf $(dirname $(which firecracker))
 rm -rf $(dirname $(which nats-server))
